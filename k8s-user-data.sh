80a5d375b82bff0bc19ca06f905d704b3d00c0f0
sqlstad <sanderstad@gmail.com>
2020-08-14 12:56:30 +0200

    Changed the kubernetes file list

--- k8s-user-data.sh
+++ k8s-user-data.sh
@@ -21,13 +21,12 @@ sudo apt-get update
 
 sudo apt-get install -yq docker-ce
 
-
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update -y
 
