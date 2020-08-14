37142317d9a08691659996311ac1f71e3412c1d5
sqlstad <sanderstad@gmail.com>
2020-08-14 13:17:01 +0200

    Colorized output

--- k8s-user-data.sh
+++ k8s-user-data.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+#echo "${red}Failure.${reset}"
+
+echo "${green}Installing wget${reset}"
 sudo apt-get update
 sudo apt-get install -y git wget
 
 # Install Docker
+echo "${green}Installing docker${reset}"
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -11,7 +19,9 @@ sudo apt-get install -y \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo apt-key fingerprint 0EBFCD88
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -21,8 +31,9 @@ sudo apt-get update
 
 sudo apt-get install -yq docker-ce
 
+# Install kubernetes
+echo "${green}Installing kubernetes${reset}"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 
@@ -33,6 +44,7 @@ sudo apt-get update -y
 sudo apt-get install -y  kubelet kubeadm kubectl kubernetes-cni nfs-common
 
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
+
 sudo swapoff -a
 
 sudo rm -rf /var/lib/kubelet/*
