d3dcfaf81c1c725a7ecbf99a65c7907270af3b36
sqlstad <sanderstad@gmail.com>
2020-08-14 13:09:49 +0200

    Changed export to file to tee

--- k8s-user-data.sh
+++ k8s-user-data.sh
@@ -26,7 +26,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update -y
 
